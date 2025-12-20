8c1320bf934a74936c7a0ce7d173c513a8ed6b31
robinlant <robinlant@proton.me>
2025-12-20 12:15:21 +0100

    solve day 4 problem 1

--- aoc/four.go
+++ aoc/four.go
@@ -1,10 +1,12 @@
 package aoc
 
 import (
-	"bytes"
-	"errors"
+	"fmt"
 	"strconv"
 )
+
+const empty = '.'
+const roll = '@'
 
 type Day4Solver struct{}
 
@@ -13,10 +15,19 @@ func (d *Day4Solver) GetDay() uint8 {
 }
 
 func (d *Day4Solver) SolveOne(i []byte) (string, error) {
-	const empty = '.'
-	const roll = '@'
+	matrix := getD4Matrix(i)
 
 	sum := 0
+	for rIndex, row := range matrix {
+		for cIndex, char := range row {
+			if char == '@' &&
+				countAdjacentRolls(matrix, rIndex, cIndex) < 4 {
+				sum++
+				fmt.Printf("row: %d, col: %d\n", rIndex+1, cIndex+1)
+			}
+		}
+	}
+
 	return strconv.Itoa(sum), nil
 }
 
@@ -24,13 +35,33 @@ func (d *Day4Solver) SolveTwo(i []byte) (string, error) {
 	return "", nil
 }
 
-func getD4Matrix(i []byte) ([][]byte, error) {
-	width := bytes.IndexAny(i, "\n")
-	if width < 1 {
-		return "", errors.New("unexpected problem format: new line as the first character")
-	}
+func getD4Matrix(i []byte) [][]byte {
 	matrix := [][]byte{}
 	for r := range rows(i) {
-
+		matrix = append(matrix, r)
 	}
+	return matrix
+}
+
+func countAdjacentRolls(m [][]byte, r, c int) uint8 {
+	var count uint8 = 0
+
+	for rIndex := r - 1; rIndex <= r+1; rIndex++ {
+		if rIndex < 0 || rIndex > len(m)-1 {
+			continue
+		}
+		for cIndex := c - 1; cIndex <= c+1; cIndex++ {
+			if cIndex < 0 ||
+				cIndex > len(m[0])-1 ||
+				(cIndex == c && rIndex == r) {
+				continue
+			}
+			char := m[rIndex][cIndex]
+			if char == roll {
+				count++
+			}
+		}
+	}
+
+	return count
 }
--- aoc/helper.go
+++ aoc/helper.go
@@ -22,9 +22,23 @@ func rows(input []byte) iter.Seq[[]byte] {
 	}
 }
 
+func enumerate[V any](seq iter.Seq[V]) iter.Seq2[int, V] {
+	return func(yield func(int, V) bool) {
+		i := 0
+		for v := range seq {
+			if !yield(i, v) {
+				return
+			}
+			i++
+		}
+	}
+}
+
 func digiByteToInt(b byte) (int, error) {
 	if b < '0' || b > '9' {
 		return 0, fmt.Errorf("%c is not a digit", b)
 	}
 	return int(b - '0'), nil
 }
+
+func unused[V any](v ...V) {}
